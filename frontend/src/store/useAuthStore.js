6416abfb9b5099ae36e24476a66ee6c313b8fe74
lance4444 <chilance44@gmail.com>
2025-02-16 23:48:11 +0800

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001"
-    : "fullstack-chatty-api.vercel.app";
+    : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
